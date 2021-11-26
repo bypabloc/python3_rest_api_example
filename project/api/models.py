eea3dcf260e24bf35748202d5b3511b5052ebfe2
Your Name <pacg1991@gmail.com>
2021-11-26 12:15:48 -0500

    added lib postgresql and migrations

--- project/api/models.py
+++ project/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    url = models.CharField(max_length=100)
+    city = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': 'python3_rest_api_example_db',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'PORT': 5432,
     }
 }
 
